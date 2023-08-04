f1500760bc2995ff7e8f0e3681e098b7aba7d294
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-08-04 16:02:46 -0400

    styles: add default text-color

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,15 @@ import menuIconImg from "@/assets/menu-icon.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between items-center h-16 bg-primary px-4 md:px-12">
-      <div className="inline-flex">
-        <h3 className="font-bold uppercase">Harbour.Space</h3>
-        <span className="ml-2 text-sm uppercase">/Interaction Design</span>
-      </div>
-      <div className="">
-        <img src={menuIconImg} width={35} height={30} alt="Menu Icon" />
+    <nav className="bg-primary px-4 md:px-12">
+      <div className="h-16 flex justify-between items-center container max-w-7xl mx-auto">
+        <div className="inline-flex">
+          <h3 className="text-white font-bold uppercase">Harbour.Space</h3>
+          <span className="text-white ml-2 text-sm uppercase">/Interaction Design</span>
+        </div>
+        <div className="">
+          <img src={menuIconImg} width={35} height={30} alt="Menu Icon" />
+        </div>
       </div>
     </nav>
   );
