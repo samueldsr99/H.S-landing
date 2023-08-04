6226446a9966809d53313b8013bf46cb3059e774
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-08-04 16:07:01 -0400

    feat(StickyBar): add

---
+++ src/components/StickyBar/StickyBar.tsx
@@ -0,0 +1,55 @@
+import { useMemo } from "react";
+import { format } from "date-fns";
+
+import useGetDataScienceApprenticeshipZeptolab from "@/hooks/useGetDataScienceApprenticeshipZeptolab";
+import { DEFAULT_DATE_FORMAT } from "@/utils/dates";
+
+import Countdown from "../Countdown/Countdown";
+
+import Entry from "./Entry";
+
+export interface StickyBarProps {}
+
+const StickyBar: React.FC<StickyBarProps> = () => {
+  const { data, isLoading } = useGetDataScienceApprenticeshipZeptolab();
+
+  const startDate = useMemo(
+    () =>
+      data?.scholarship?.scholarshipStartDate &&
+      format(new Date(data?.scholarship?.scholarshipStartDate), DEFAULT_DATE_FORMAT),
+    [data?.scholarship?.scholarshipStartDate],
+  );
+
+  const applicationDeadline = useMemo(
+    () =>
+      data?.scholarship?.applicationEndDate &&
+      format(new Date(data?.scholarship?.applicationEndDate), DEFAULT_DATE_FORMAT),
+    [data?.scholarship?.applicationEndDate],
+  );
+
+  if (isLoading) {
+    return <></>;
+  }
+
+  return (
+    <div className="hidden lg:block fixed bottom-0 left-0 right-0 border-t border-[#DADADA]">
+      <div className="container max-w-7xl mx-auto">
+        <div className="bg-white shadow-sm px-2 pt-6 pb-4 flex justify-around">
+          <Entry title={data?.scholarship.company.name} description={data?.scholarship.company.type} />
+          <Entry title="Location" description={data?.scholarship.location.name} />
+          <Entry title="Duration" description={`${data?.scholarship?.duration} Year Full-Time`} />
+          <Entry title="Start date" description={startDate} />
+          <Entry title="Application deadline" description={applicationDeadline} />
+          {data?.scholarship?.applicationEndDate && (
+            <Entry
+              title="Application closes in"
+              description={<Countdown targetDate={new Date(data?.scholarship?.applicationEndDate)} />}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StickyBar;
