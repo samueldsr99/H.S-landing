1ec3d27d8ef92598c307a4e4e56eca821291dac1
Samuel David Suarez Rodriguez <42628316+samueldsr99@users.noreply.github.com>
2023-08-04 13:32:32 -0400

    Setup tests (#5)
    * feat: setup tests
    
    * test(useTimer): add
    
    * chore: add `tsconfig.spec.json`
    
    * chore: add config for vitest
    
    * chore: add coverage support
    
    * bugfix: declare test functions
    
    * bugfix: fix build errors

---
+++ src/hooks/useTimer.test.ts
@@ -0,0 +1,35 @@
+import { addDays, addHours, addMinutes, addSeconds } from "date-fns";
+import { describe, expect, it } from "vitest";
+
+import { calculateTimeLeft } from "./useTimer";
+
+describe("", () => {
+  it("should return 0 when new Date() is now", () => {
+    const now = new Date();
+    const { days, hours, minutes, seconds } = calculateTimeLeft(now);
+    expect([days, hours, minutes, seconds]).toEqual([0, 0, 0, 0]);
+  });
+
+  it("should work properly on future dates", () => {
+    const now = new Date();
+
+    let futureDate = addDays(now, 7);
+    futureDate = addHours(futureDate, 20);
+    futureDate = addMinutes(futureDate, 38);
+
+    const { days, hours, minutes, seconds } = calculateTimeLeft(futureDate);
+    expect([days, hours, minutes, seconds]).toEqual([7, 20, 38, 0]);
+  });
+
+  it("should respect time conversions properly", () => {
+    const now = new Date();
+
+    let futureDate = addDays(now, 7);
+    futureDate = addHours(futureDate, 28);
+    futureDate = addMinutes(futureDate, 80);
+    futureDate = addSeconds(futureDate, 90);
+
+    const { days, hours, minutes, seconds } = calculateTimeLeft(futureDate);
+    expect([days, hours, minutes, seconds]).toEqual([8, 5, 21, 30]);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,11 @@ export default defineConfig({
     alias: {
       "@": resolve(__dirname, "src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["**/*.test.{ts,tsx}"],
+    setupFiles: "./src/tests/setup.ts",
   },
 });
