ce2389f5e3beaa185f3d587fcd0fab159268346d
Samuel David Suarez Rodriguez <42628316+samueldsr99@users.noreply.github.com>
2023-08-04 20:46:58 -0400

    Add FAQ (#8)
    * feat: add accordion icons
    
    * feat(Select): add
    
    * feat(FAQItem): add
    
    * bugfix: fix typo
    
    * feat(FAQ): add
    
    * styles(FAQItem): apply borders

---
+++ src/components/FAQItem/FAQItem.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import clsx from "classnames";
+
+export type FAQItemProps = JSX.IntrinsicElements["div"] & {
+  type: string;
+  question: string;
+  answer: string;
+};
+
+const FAQItem: React.FC<FAQItemProps> = ({ className, type, question, answer, ...props }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div className={clsx("flex justify-between items-start py-8 border-b border-[#E6E6E6]", className)} {...props}>
+      <h4 className="w-[34%] text-lg hidden md:block shrink-0 text-primary md:text-xl font-medium leading-8">{type}</h4>
+      <div className="w-full md:w-[58%] mr-12">
+        <h4 className="text-lg font-light leading-8 md:font-medium md:text-xl">{question}</h4>
+        {open && <p className="mt-8 font-light leading-8 text-base md:text-xl">{answer}</p>}
+      </div>
+      <button className="shrink-0 outline-none place-self-end self-start" onClick={() => setOpen((prev) => !prev)}>
+        <img
+          className="w-12 h-12"
+          src={open ? "/accordion-opened.svg" : "/accordion-closed.svg"}
+          alt="Accordion Button"
+          width={48}
+          height={48}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default FAQItem;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import About from "@/page-components/About/About";
+import FAQ from "@/page-components/FAQ/FAQ";
 
 const HomePage: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage: React.FC = () => {
       </header>
       <main className="pb-8 md:pb-20 lg:pb-36">
         <About />
+        <FAQ />
       </main>
     </>
   );
