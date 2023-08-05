34fa1d207fb3019013c65fbaa9ec3d03fd91053a
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-08-04 23:24:25 -0400

    styles(About): fix responsive margins

--- src/page-components/About/About.tsx
+++ src/page-components/About/About.tsx
@@ -13,7 +13,7 @@ const About: React.FC = () => {
         <div className="flex flex-col items-center md:flex-row justify-between">
           <div className="flex-container">
             <div className="image-container" />
-            <div className="relative z-10 bg-white border px-4 py-2 w-full pt-52 md:mx-0 md:border-none md:pt-16 sm:pl-10 md:pl-28 space-y-10">
+            <div className="relative z-10 bg-white border px-4 py-2 w-full sm:pt-16 sm:pl-10 md:mx-0 md:border-none lg:pt-16 lg:pl-28 space-y-10">
               <h1 className="text-primary text-4xl md:text-5xl font-medium leading-10">About the apprenticeship</h1>
               <p className="text-lg md:text-xl font-light leading-8">{data?.scholarship?.about?.[0]?.data}</p>
             </div>
