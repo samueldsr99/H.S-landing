c6dbc5f77243146b5dbeb67808d25196ae80ea17
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-08-04 22:41:37 -0400

    bugfix(Card): minor fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,12 +3,12 @@ import clsx from "classnames/bind";
 export type CardProps = JSX.IntrinsicElements["article"];
 
 export type CardEntryProps = JSX.IntrinsicElements["div"] & {
-  title: string;
-  content: string;
+  title?: string;
+  content?: string;
 };
 
 const classes = clsx.bind({
-  "card-container": "border border-[#DADADA] rounded-[4px] px-6 pt-8 pb-4",
+  "card-container": "bg-white border border-[#DADADA] rounded-[4px] px-6 pt-8 pb-4",
   "title-label": "text-primary text-sm font-medium",
   content: "text-[#535353] text-sm font-light leading-6",
 });
